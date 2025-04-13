bca43adfb1ec19482f6002629707e0855f39cca7
Fırat Dalkılıç <firatdalkilic87@gmail.com>
2025-04-14 01:36:46 +0300

    Şifre değiştirme sayfası config dosya yolu düzeltildi

--- admin/change-password.php
+++ admin/change-password.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== tru
     exit;
 }
 
-require_once '../config/db_connection.php';
+require_once 'config.php';
 
 $success_message = '';
 $error_message = '';
