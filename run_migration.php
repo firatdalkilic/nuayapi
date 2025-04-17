8352fd0c005f0d753dbcc28502396d7d8dc52583
Fırat Dalkılıç <firatdalkilic87@gmail.com>
2025-04-17 04:19:12 +0300

    JawsDB bağlantı scripti eklendi

---
+++ run_migration.php
@@ -0,0 +1,22 @@
+<?php
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+// Create connection
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Run the migration script
+require_once('add_features_column.php');
+
+$conn->close();
+?> 
